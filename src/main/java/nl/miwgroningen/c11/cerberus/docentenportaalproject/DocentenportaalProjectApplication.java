ddf5b98f0afa963a29ca50873e8f3157978939b3
Joost Schreuder <jschreuder92@gmail.com>
2023-06-12 14:16:45 +0200

    initial commit

---
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/DocentenportaalProjectApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.c11.cerberus.docentenportaalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocentenportaalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocentenportaalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/DocentenportaalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.c11.cerberus.docentenportaalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocentenportaalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
