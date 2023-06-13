f9f6a7dc5a6db09bc0c5ad9f2548449f3969111c
Marianne <Marianne-Kooistra@hotmail.com>
2023-06-13 09:46:08 +0200

    Added student part: repository, controller with overview new and edit, html pages overview and form

---
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/StudentController.java
@@ -0,0 +1,58 @@
+package nl.miwgroningen.c11.cerberus.docentenportaalproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Student;
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Subject;
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.repository.StudentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentRepository studentRepository;
+
+    @GetMapping("/student/all")
+    private String showStudentOverview(Model model) {
+        model.addAttribute("allStudents", studentRepository.findAll());
+
+        return "studentOverview";
+    }
+
+    @GetMapping("/student/new")
+    private String showCreateStudentForm(Model model) {
+        model.addAttribute("subject", new Subject());
+
+        return "createStudentForm";
+    }
+
+    @GetMapping("/student/edit/{studentId}")
+    private String showEditStudentForm(@PathVariable("studentId") Long studentId, Model model) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+
+        if (optionalStudent.isPresent()) {
+            model.addAttribute("student", optionalStudent.get());
+            return "createStudentForm";
+        }
+
+        return "redirect:/student/all";
+    }
+
+    @PostMapping("/student/new")
+    private String saveOrUpdateStudent(@ModelAttribute("student") Student studentToBeSaved, BindingResult result) {
+
+        if (!result.hasErrors()) {
+            studentRepository.save(studentToBeSaved);
+        }
+
+        return "redirect:/student/all";
+    }
+
+}
---
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.c11.cerberus.docentenportaalproject.repository;
+
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
