41cce648504dcdf52594e6d2e09912eb9fff178c
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-06-14 12:18:25 +0200

    conflict solved

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/ProgrammeController.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/ProgrammeController.java
@@ -10,10 +10,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
+/**
+ * Handels all actions concerning programme.
+ * Gemaakt door Marianne Kooistra (me.kooistra@st.hanze.nl) op 14/06/2023
+ */
+
 @Controller
 @RequiredArgsConstructor
 public class ProgrammeController {
     private final ProgrammeRepository programmeRepository;
+
+    @GetMapping("/home")
+    // TODO: "/home" needs to be just "/" but but subjects/all still has that.
+    private String showHomePage(Model model) {
+        model.addAttribute("allProgrammes", programmeRepository.findAll());
+
+        return "HomePage";
+    }
+
+    // TODO: new and edit?
+
+    @GetMapping("/home/delete/{programmeId}")
+    private String deleteProgramme(@PathVariable("programmeId") Long programmeId) {
+        Optional<Programme> optionalProgramme = programmeRepository.findById(programmeId);
+
+        if (optionalProgramme.isPresent()) {
+            programmeRepository.delete(optionalProgramme.get());
+        }
+        
+        return "redirect:/home";
+    }
 
     @GetMapping("/programme/view/{programmeId}")
     private String showProgrammeDetails(@PathVariable("programmeId") Long programmeId, Model model) {
