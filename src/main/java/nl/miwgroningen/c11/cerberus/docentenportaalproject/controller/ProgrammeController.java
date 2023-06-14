436dd9311a7217626eb3435b07b4baf34e64fcb9
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-06-14 11:57:38 +0200

    added programme details page

---
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/ProgrammeController.java
@@ -0,0 +1,6 @@
+package nl.miwgroningen.c11.cerberus.docentenportaalproject.controller;/**
+ * A
+ *
+ * @author Marianne Kooistra, Mohammed Almameri, Joost Schreuder 14/06/2023
+ */public class ProgrammeController {
+}
