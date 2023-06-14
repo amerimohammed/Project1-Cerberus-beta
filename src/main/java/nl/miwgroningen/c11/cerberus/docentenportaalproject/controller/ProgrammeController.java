eb84375cf041d340316b15787ff9b7b645968603
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-06-14 11:58:43 +0200

    added programme details page

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/ProgrammeController.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/controller/ProgrammeController.java
@@ -1,6 +1,28 @@
-package nl.miwgroningen.c11.cerberus.docentenportaalproject.controller;/**
- * A
- *
- * @author Marianne Kooistra, Mohammed Almameri, Joost Schreuder 14/06/2023
- */public class ProgrammeController {
+package nl.miwgroningen.c11.cerberus.docentenportaalproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Programme;
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.repository.ProgrammeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class ProgrammeController {
+    private final ProgrammeRepository programmeRepository;
+
+    @GetMapping("/programme/view/{programmeId}")
+    private String showProgrammeDetails(@PathVariable("programmeId") Long programmeId, Model model) {
+        Optional<Programme> optionalProgramme = programmeRepository.findById(programmeId);
+        if (optionalProgramme.isPresent()) {
+            model.addAttribute("programme", optionalProgramme.get());
+            return "programme/programmeDetails";
+        }
+        return "redirect:/programme/all";
+    }
+
 }
