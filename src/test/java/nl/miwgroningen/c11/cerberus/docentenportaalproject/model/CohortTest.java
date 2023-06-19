69e4360e67beea34d360beaefbfeb627e934213c
Joost Schreuder <jschreuder92@gmail.com>
2023-06-19 13:18:06 +0200

    minor refactoring cohort setDate tests

--- src/test/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/model/CohortTest.java
+++ src/test/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/model/CohortTest.java
@@ -11,50 +11,52 @@ import static org.junit.jupiter.api.Assertions.*;
     * @author Joost Schreuder
 */class CohortTest {
 
+    private static final LocalDate TEST_DATE = LocalDate.parse("1999-01-01");
+
     //Tests whether error is thrown when end-date is before start-date
     @Test
     void setStartDate() {
         Cohort cohort = new Cohort();
 
-        //Test whether end-date can be set when there is no start-date yet
-        assertDoesNotThrow(() -> cohort.setEndDate(LocalDate.parse("1999-01-01")));
+        //Test whether endDate can be set when there is no startDate yet
+        assertDoesNotThrow(() -> cohort.setEndDate(TEST_DATE));
 
-        //Test for end-date and start-date on same day
-        assertThrows(IllegalArgumentException.class, () -> cohort.setStartDate(LocalDate.parse("1999-01-01")));
+        //EndDate and StartDate cannot be the same
+        assertThrows(IllegalArgumentException.class, () -> cohort.setStartDate(TEST_DATE));
 
-        //Test for setting start-date after end-date
-        assertThrows(IllegalArgumentException.class, () -> cohort.setStartDate(LocalDate.parse("1999-01-02")));
+        //StartDate cannot be after endDate
+        assertThrows(IllegalArgumentException.class, () -> cohort.setStartDate(TEST_DATE.plusDays(1)));
 
-        //Test for setting start-date to null
+        //StartDate cannot be set to null
         assertThrows(NullPointerException.class, () -> cohort.setStartDate(null));
 
-        //Test whether no error is thrown if start-date is set before end-date
-        assertDoesNotThrow(() -> cohort.setStartDate(LocalDate.parse("1998-12-31")));
+        //StartDate can be set before endDate
+        assertDoesNotThrow(() -> cohort.setStartDate(TEST_DATE.minusDays(1)));
 
         //Check whether date is set right
-        assertEquals(cohort.getStartDate(), LocalDate.parse("1998-12-31"));
+        assertEquals(cohort.getStartDate(), TEST_DATE.minusDays(1));
     }
 
     @Test
     void setEndDate() {
         Cohort cohort = new Cohort();
 
-        //Test whether end-date can be set when there is no start-date yet
-        assertDoesNotThrow(() -> cohort.setStartDate(LocalDate.parse("1999-01-02")));
+        //Test whether endDate can be set when there is no startDate yet
+        assertDoesNotThrow(() -> cohort.setStartDate(TEST_DATE));
 
-        //Test for end-date and start-date on same day
-        assertThrows(IllegalArgumentException.class, () -> cohort.setEndDate(LocalDate.parse("1999-01-02")));
+        //endDate and startDate cannot be on the same day
+        assertThrows(IllegalArgumentException.class, () -> cohort.setEndDate(TEST_DATE));
 
-        //Test for setting end-date before start-date
-        assertThrows(IllegalArgumentException.class, () -> cohort.setEndDate(LocalDate.parse("1999-01-01")));
+        //endDate cannot be before startDate
+        assertThrows(IllegalArgumentException.class, () -> cohort.setEndDate(TEST_DATE.minusDays(1)));
 
-        //Test for setting end-date to null
+        //endDate cannot be set to null
         assertThrows(NullPointerException.class, () -> cohort.setEndDate(null));
 
-        //Test whether no error is thrown if end-date is set after start-date
-        assertDoesNotThrow(() -> cohort.setEndDate(LocalDate.parse("1999-01-03")));
+        //endDate can be after startDate
+        assertDoesNotThrow(() -> cohort.setEndDate(TEST_DATE.plusDays(1)));
 
         //Check whether date is set right
-        assertEquals(cohort.getEndDate(), LocalDate.parse("1999-01-03"));
+        assertEquals(cohort.getEndDate(), TEST_DATE.plusDays(1));
     }
 }
