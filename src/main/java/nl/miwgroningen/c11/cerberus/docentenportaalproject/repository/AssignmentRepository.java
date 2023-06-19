2f238314352feaab628234d623d2322599a71e8c
Marianne <Marianne-Kooistra@hotmail.com>
2023-06-19 11:56:30 +0200

    Removed forgotten comments

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/AssignmentRepository.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/AssignmentRepository.java
@@ -2,8 +2,5 @@ package nl.miwgroningen.c11.cerberus.docentenportaalproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Gemaakt door Marianne Kooistra (me.kooistra@st.hanze.nl) op 19/06/2023
- */
 public interface AssignmentRepository extends JpaRepository {
 }
