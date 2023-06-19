1c3a03a8a2587d5ed2aa66d39c3f77f47b67dbcf
Marianne <Marianne-Kooistra@hotmail.com>
2023-06-19 12:01:20 +0200

    Forgot something small in repository (sorry for the many commits)

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/AssignmentRepository.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/AssignmentRepository.java
@@ -1,6 +1,7 @@
 package nl.miwgroningen.c11.cerberus.docentenportaalproject.repository;
 
+import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssignmentRepository extends JpaRepository {
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
 }
