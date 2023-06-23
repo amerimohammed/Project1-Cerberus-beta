1e51a4ca502b7d6ceeed6d66a7ebc5da232075fc
Mohammed Alameri <mohammedagl6@gmail.com>
2023-06-23 13:30:20 +0200

    removed repeated Cohorts by DISTINCT when querying Cohorts related to a teacher

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/CohortRepository.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/repository/CohortRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CohortRepository extends JpaRepository<Cohort, Long> {
-    @Query("SELECT c FROM Cohort c JOIN c.programme p JOIN p.subjects s JOIN s.teachers t WHERE t.username = ?1")
+    @Query("SELECT DISTINCT c FROM Cohort c JOIN c.programme p JOIN p.subjects s JOIN s.teachers t WHERE t.username = ?1")
     List<Cohort> findCohortsByTeacherUsername(String username);
 }
