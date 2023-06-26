0bc133567a82f2079e3847591a343a09898e373a
Marianne <Marianne-Kooistra@hotmail.com>
2023-06-26 15:50:34 +0200

    made test and test parts editable

--- src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/dto/subTestDTO.java
+++ src/main/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/dto/subTestDTO.java
@@ -2,9 +2,11 @@ package nl.miwgroningen.c11.cerberus.docentenportaalproject.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test;
 
 @Data @Builder
+@Getter
 public class subTestDTO {
     private Test superTest;
     private Test subTest;
