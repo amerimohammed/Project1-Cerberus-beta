5b60ef60de780175f55e7ea860d96ce5c1c64a35
Marianne <Marianne-Kooistra@hotmail.com>
2023-06-28 10:41:02 +0200

    Finished tests for test display date

--- src/test/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/model/TestModelTest.java
+++ src/test/java/nl/miwgroningen/c11/cerberus/docentenportaalproject/model/TestModelTest.java
@@ -1,6 +1,5 @@
 package nl.miwgroningen.c11.cerberus.docentenportaalproject.model;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,31 +12,29 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class TestModelTest {
 
-    @Test
-    @DisplayName("DisplayTestDate shows no date when date is null")
-    void DisplayDateNoDateWhenNull() {
-        nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test test = new nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test();
-        test.setTestDate(null);
-
-        assertEquals("No date", test.displayTestDate());
-    }
-
-    //TODO: invalid date test-> month 13 + day 31 of february + day 32 of any other month.
-    @Test
-    @Disabled
-    @DisplayName("DisplayTestDate gives error when date is invalid")
-    void displayDateGivesErrorWhenDateInvalid() {
-        nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test test = new nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test();
-        test.setTestDate(LocalDate.of(2023, 13,1));
-
-    }
+    //This is a constant to avoid having to use the long adres of the Test model.
+    //(Next time when working with 'toetsen' give them a different name)
+    private static final nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test TEST = new nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test();
 
     @Test
     @DisplayName("DisplayTestDate shows formatted date")
     void displayDateShowsFormattedDate() {
-        nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test test = new nl.miwgroningen.c11.cerberus.docentenportaalproject.model.Test();
-        test.setTestDate(LocalDate.of(2023, 01, 21));
+        TEST.setTestDate(LocalDate.of(2023,01, 21));
 
-        assertEquals("21 - 01 - 2023", test.displayTestDate());
+        assertEquals("21 - 01 - 2023", TEST.displayTestDate());
     }
+
+    @Test
+    @DisplayName("DisplayTestDate shows no date when date is null")
+    void DisplayDateNoDateWhenNull() {
+        setTestToAllNull();
+        assertEquals("No date", TEST.displayTestDate());
+    }
+
+
+    //Method to be able to reuse the same constant
+    private static void setTestToAllNull() {
+        TEST.setTestDate(null);
+    }
+
 }
